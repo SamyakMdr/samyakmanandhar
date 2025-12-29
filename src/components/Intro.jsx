a7eb76f5733569f24e6bda9aaca764795a87fa3a
SamyakMdr <samyak11manandhar@gmail.com>
2025-12-29 09:13:16 +0545

    Update PDF link to use root-relative path

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -64,7 +64,7 @@ const Intro = () => {
               </button>
             </Link>
             <a
-              href="public/SamyakManandhar.pdf"
+              href="/SamyakManandhar.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-block"
